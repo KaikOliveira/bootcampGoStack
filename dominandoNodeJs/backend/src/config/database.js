2a3c295dc9038e5b269b257fec143633b5dd3de6
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-26 03:14:17 -0300

    errMigrate

---
+++ dominandoNodeJs/backend/src/config/database.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'kaiko',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
