d99503afcc1685c4890f0b2488793a2800bf290a
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-27 05:03:09 -0300

    module02OK

--- dominandoNodeJs/backend/src/app/controllers/SessionController.js
+++ dominandoNodeJs/backend/src/app/controllers/SessionController.js
@@ -1,10 +1,25 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
+
+    // Function CREATE Session
     async store(req, res) {
+
+      // Vadalidaçoes do Yup
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().required(),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation fails' });
+      }
+
+      // Buscando email e password do Body
       const { email, password } = req.body;
 
       // Verificação se existe usuario com o email
--- dominandoNodeJs/backend/src/app/controllers/UserController.js
+++ dominandoNodeJs/backend/src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
+
+  // Function CREATE USER
   async store(req, res) {
+
+    // Vadalidaçoes do Yup
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+
     // Tratamento de erro = EMAIL ja existente
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
@@ -21,7 +38,26 @@ class UserController {
     });
   }
 
+  // Function UPDATE USER
   async update(req, res) {
+
+    // Vadalidaçoes do Yup
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     // Buscando email e Password atual/antiga
     const { email, oldPassword } = req.body;
 
