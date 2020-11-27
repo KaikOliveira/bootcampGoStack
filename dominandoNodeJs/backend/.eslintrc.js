82871b10e6432ccdb9a52a64b79371057a3cb633
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-26 21:37:48 -0300

    modelStart

--- dominandoNodeJs/backend/.eslintrc.js
+++ dominandoNodeJs/backend/.eslintrc.js
@@ -1,20 +1,18 @@
 module.exports = {
   env: {
-    es6: true,
+    es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
