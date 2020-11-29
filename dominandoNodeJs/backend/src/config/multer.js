1c0efde9d8437d020b3cf0cf51fdebe62a07fee2
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-11-29 04:10:48 -0300

    err

---
+++ dominandoNodeJs/backend/src/config/multer.js
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+
+export default {
+  // Salvar o up no \temp do app
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        // Exemplo u234asf342.png
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      })
+    }
+  }),
+}
