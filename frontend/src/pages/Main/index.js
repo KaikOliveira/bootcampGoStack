aa87bd98040ba6c16a87719f156655cba667b9b8
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-05 17:02:11 -0300

    apiRestgithub

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Main() {
-  return <h1>Main</h1>;
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Conteiner, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.get(`/repos/${newRepo}`);
+    console.log(response.data);
+  }
+
+  return (
+    <Conteiner>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+
+      <Form onSubmit={handlerSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositorio"
+          onChange={(e) => setNewRepo(e.target.value)}
+          value={newRepo}
+        />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Conteiner>
+  );
 }
-
-export default Main;
