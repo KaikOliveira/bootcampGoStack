6666878d11c26739eb7b669797dd3a1a1233b38f
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-09 23:38:46 -0300

    vadalidacaoDates

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,18 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
+// Tipagem da variaveis
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
 // Array de memoria
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  // Verificação de date disponivel
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'O Appointment nao esta disponivel.' });
+  }
+
   const appointment = {
     id: uuid(),
-    provider: String,
-    date: String,
+    provider,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
